52a4c3b8177e8adaf2bb735be8b8183f26aa3d41
abhinandanshukla0777 <shuklaabhi09787@gmail.com>
2025-07-24 23:12:38 +0530

    Initial commit

--- backend/src/models/Thread.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ThreadSchema = new mongoose.Schema({
-  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
-  title: String,
-  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
-});
-
-module.exports = mongoose.model('Thread', ThreadSchema); 
--- backend/src/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getNotifications, markAsRead } = require('../controllers/notificationController');
-const { protect } = require('../middleware/auth');
-const router = express.Router();
-
-router.get('/', protect, getNotifications);
-router.put('/:id/read', protect, markAsRead);
-
-module.exports = router; 
